9da6e238c7a5e40369a5f721a7b7f1d5de580531
CodenificienT <61930093+codenificient@users.noreply.github.com>
2022-12-11 02:47:03 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+console.log(process.env.MONGODB);
+
+mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+});
+
+const visitorSchema = new mongoose.Schema({
+  name: String,
+  count: Number,
+});
+
+const Visitor = mongoose.model("Visitor", visitorSchema);
+
+app.use(cors());
+app.use(express.json());
+
+// Get request to app root
+app.get("/", async function (req, res) {
+  // Storing the records from the Visitor table
+  let visitors = await Visitor.findOne({ name: "tioyedev" });
+
+  // If the app is being visited first
+  // time, so no records
+  if (visitors == null) {
+    // Creating a new default record
+    const beginCount = new Visitor({
+      name: "tioyedev",
+      count: 1,
+    });
+
+    // Saving in the database
+    beginCount.save();
+
+    // Sending thee count of visitor to the browser
+    res.send("1");
+
+    // Logging when the app is visited first time
+    console.log("First visitor arrived");
+  } else {
+    // Incrementing the count of visitor by 1
+    visitors.count += 1;
+
+    // Saving to the database
+    visitors.save();
+
+    // Sending thee count of visitor to the browser
+    res.send(visitors.count);
+
+    // Logging the visitor count in the console
+    console.log("visitor arrived: ", visitors.count);
+  }
+});
+
+app.get("/new/:name", async function (req, res) {
+  const newApp = req.params.name;
+  let appName = await Visitor.findOne({ name: newApp });
+  if (appName == null) {
+    const beginCount = new Visitor({
+      name: newApp,
+      count: 1,
+    });
+
+    beginCount.save();
+    res.send("1");
+  } else {
+    appName.count += 1;
+
+    // Saving to the database
+    appName.save();
+
+    // Sending thee count of visitor to the browser
+    res.send(appName.count);
+
+    // Logging the visitor count in the console
+    console.log("visitor arrived: ", appName.count);
+  }
+});
+
+// Creating server to listen at localhost 3000
+app.listen(PORT, function (req, res) {
+  // Logging when the server has started
+  console.log("listening to server " + PORT);
+});
