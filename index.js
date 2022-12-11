951894739c0c6e2ca21f178f9e1aa58f12502618
CodenificienT <61930093+codenificient@users.noreply.github.com>
2022-12-11 02:58:43 -0500

    set mongoose strict

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-
+mongoose.set("strictQuery", true);
 mongoose.connect(process.env.MONGODB, {
   useNewUrlParser: true,
 });
